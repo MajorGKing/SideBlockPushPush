be64d67eaa877c5eb9f7d47583889a57bf2a9133
MajorGKing <majorgking@gmail.com>
2024-08-18 00:43:34 +0900

    2d에 대한 터치입력 되는 부분, UI 터치는 반응하지 않도록 추가

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void LateUpdate()
@@ -36,13 +37,30 @@ public class CameraController : MonoBehaviour
         }
         else if (_mode == Define.CameraMode.NormalView)
         {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Vector2 touchPosition = Input.GetTouch(0).position;
 
+                // Check if the touch is over a UI element
+                int _index = (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) ? 0 : -1;
+                if (EventSystem.current.IsPointerOverGameObject(_index))
+                {
+                    // Touch is on a UI element, so do nothing
+                    return;
+                }
+
+                // Log or process the touch position here if needed
+                Debug.Log("Touch Position: " + touchPosition);
+
+                // Convert touch position to a ray
+                Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+                RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.transform.name);
+                }
+            }
         }
-    }
-
-    public void SetQuarterView(Vector3 delta)
-    {
-        _mode = Define.CameraMode.QuarterView;
-        _delta = delta;
     }
 }
